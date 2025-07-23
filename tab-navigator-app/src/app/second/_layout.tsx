1d96cce6a755e8fdacd24aa4c5b227da1c5ffd7c
yuichikawano <50235941+yuichikawano@users.noreply.github.com>
2025-07-23 14:11:38 +0900

    タブの移動とStack内の移動でアニメーションを変更する

--- tab-navigator-app/src/app/second/_layout.tsx
+++ tab-navigator-app/src/app/second/_layout.tsx
@@ -1,12 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack, usePathname } from "expo-router";
 
 /**
  * ここにStackを配置することで、上のナビゲーションと1個下の階層としてStackが作成される
  * →上のTabナビゲーションにこのフォルダの要素が追加されない。
  */
 export default function Layout() {
+  const pathname = usePathname();
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        animation: pathname.startsWith("/second") ? "default" : "none",
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Second Screen" }} />
       <Stack.Screen name="nested" options={{ title: "Second Nested Screen" }} />
       <Stack.Screen
