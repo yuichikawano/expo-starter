47f547eca38c1a018c38abf502f592e8ac940ec1
yuichikawano <50235941+yuichikawano@users.noreply.github.com>
2025-07-23 13:26:56 +0900

    タブスクリーンのタブのレイアウト設定・画面タイトルの設定

--- tab-navigator-app/src/app/_layout.tsx
+++ tab-navigator-app/src/app/_layout.tsx
@@ -2,16 +2,66 @@ import { Tabs } from "expo-router";
 import "../../global.css";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function RootLayout() {
   return (
     <React.Fragment>
       <StatusBar style="auto" />
-      <Tabs>
-        <Tabs.Screen name="index" options={{ title: "Index" }} />
-        <Tabs.Screen name="second" options={{ title: "Second" }} />
-        <Tabs.Screen name="third" options={{ title: "Third" }} />
-        <Tabs.Screen name="fourth" options={{ title: "Fourth" }} />
+      <Tabs screenOptions={{ tabBarActiveTintColor: "teal" }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="numeric-1-box-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="second"
+          options={{
+            title: "Second",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="numeric-2-box-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="third"
+          options={{
+            title: "Third",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="numeric-3-box-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="fourth"
+          options={{
+            title: "Fourth",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="numeric-4-box-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
       </Tabs>
     </React.Fragment>
   );
