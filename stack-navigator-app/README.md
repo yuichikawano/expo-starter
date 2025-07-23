2a3a33d3b76d4d924241344b0a4620c1c83332c0
yuichikawano <50235941+yuichikawano@users.noreply.github.com>
2025-07-23 10:59:41 +0900

    create Stack Navigator Project

---
+++ stack-navigator-app/README.md
@@ -0,0 +1,20 @@
+# StackNavigatorの概要
+
+#### create project
+```zsh
+npx create-expo-app stack-navigator-app --template https://github.com/kadikraman/expo-starter
+
+```
+#### install
+```zsh
+npm i
+```
+
+#### 起動方法
+```zsh
+npm run start
+```
+#### sitemapの表示
+```zsh
+ npx expo-router-sitemap
+```
---
+++ stack-navigator-app/src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+import "../../global.css";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <React.Fragment>
+      <StatusBar style="auto" />
+      <Stack />
+    </React.Fragment>
+  );
+}
