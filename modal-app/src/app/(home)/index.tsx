ad24079939324e320a2ad9909c11c2d1bc6751f1
yuichikawano <50235941+yuichikawano@users.noreply.github.com>
2025-07-23 14:29:29 +0900

    React-NativeのAlertダイアログ

--- modal-app/src/app/(home)/index.tsx
+++ modal-app/src/app/(home)/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { AppText } from "@/components/AppText";
 import { Button } from "@/components/Button";
 import { Link, useRouter } from "expo-router";
@@ -6,6 +6,21 @@ import { Link, useRouter } from "expo-router";
 export default function IndexScreen() {
   const router = useRouter();
   const canGoBack = router.canGoBack();
+  const handleOpenAlert = () => {
+    // Alert.alert("Alert Title");
+    Alert.alert("Alert Title", "Alert Message", [
+      {
+        text: "Cancel",
+        style: "cancel",
+        onPress: () => console.log("Cancel Pressed"),
+      },
+      {
+        text: "OK",
+        onPress: () => console.log("OK Pressed"),
+        style: "destructive",
+      },
+    ]);
+  };
   return (
     <View className="justify-center flex-1 p-4">
       <AppText center>Index Screen</AppText>
@@ -21,6 +36,13 @@ export default function IndexScreen() {
           }}
         />
       ) : null}
+      <Button
+        title="Open Alert"
+        theme="secondary"
+        onPress={() => {
+          handleOpenAlert();
+        }}
+      />
     </View>
   );
 }
