9a6c5c06c9f6e7381c37635821a328700e591b8a
yuichikawano <50235941+yuichikawano@users.noreply.github.com>
2025-07-23 10:44:12 +0900

    LayoutコンポーネントでSlotコンポーネントがないと中の画面が見れなくなる

--- expo-starter1/src/app/(anything)/(fourth)/fifth/sixth/_layout.tsx
+++ expo-starter1/src/app/(anything)/(fourth)/fifth/sixth/_layout.tsx
@@ -1,9 +1,22 @@
 import { Slot } from "expo-router";
+import { AppText } from "@/components/AppText";
+import { View } from "react-native";
 
 /**
  * Slot: 各画面を挿入するためのコンポーネント
  * @constructor
  */
 export default function Layout() {
-  return <Slot />;
+  // return <Slot />;
+
+  /**
+   * LayoutコンポーネントでSlotコンポーネントがないと中の画面が見れなくなる。
+   */
+  return (
+    <View>
+      <AppText center size="heading" bold>
+        Nothing to see here!
+      </AppText>
+    </View>
+  );
 }
